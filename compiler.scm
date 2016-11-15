734a22ccfbe7b3068fcea6e3547802b69839c7ec
303111231_301598579 <avichay13@gmail.com>
2016-11-16 00:23:56 +0200

    incomplete <string>

--- compiler.scm
+++ compiler.scm
@@ -1,4 +1,7 @@
-(load "~/Chez/hw1/pc.scm")
+(load "~/Projects/Compilatzia/pc.scm")
+
+
+#;this_is_the_numeric_part-------------------------------------------------------------------------------------------------
 
 (define <digit-0-9>
   (range #\0 #\9))
@@ -52,11 +55,62 @@
 	  (/ num den)))
        done))
 
+
+#;this_is_the_string_part--------------------------------------------------------------------------------------------
   
-(define <stringVisibaleChar>
+(define <StringLiteralChar>
   (range #\  #\delete))
   
 (define <VisibleSimpleChar> 
   (range #\! #\delete))
  
+(define ^<meta-char>
+  (lambda (str ch)
+    (new (*parser (word str))
+	 (*pack (lambda (_) ch))
+	 done)))
+   
+ 
+(define <StringMetaChar>
+  (new (*parser (^<meta-char> "\\\\" #\\))
+       (*parser (^<meta-char> "\\\"" #\"))
+       (*parser (^<meta-char> "\\n" #\newline))
+       (*parser (^<meta-char> "\\r" #\return))
+       (*parser (^<meta-char> "\\t" #\tab))
+       (*parser (^<meta-char> "\\f" #\page)) ; formfeed
+       (*parser (^<meta-char> "\\{lambda}" (integer->char 955)))
+       (*parser (^<meta-char> "\\{alef}" (integer->char 1488)))
+       (*parser (^<meta-char> "\\{bismillah}" (integer->char 65021)))
+       (*parser (^<meta-char> "\\{smiley}" (integer->char 9786)))
+
+       (*disj 10)
+       done))
+
+  
+(define <StringChar>
+  (new (*parser <StringMetaChar>)
+
+       (*parser <StringLiteralChar>)
+
+       (*parser (char #\"))
+       (*parser (char #\\))
+       (*disj 2)
+
+       *diff
+       
+       #;TODO:add_a_stringHexChar_function
+       (*disj 2)
+       done))
+       
+(define <String>
+  (new (*parser (char #\"))
+       (*parser <StringChar>) *star
+       (*parser (char #\"))
+       (*caten 3)
+
+       (*pack-with
+	(lambda (open-delim chars close-delim)
+	  (list->string chars)))
+
+       done))
   
